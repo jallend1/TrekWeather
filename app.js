488de3ba02af60117ee19567ebd1b827a0328c7d
jallend1 <jallend1@gmail.com>
2019-10-27 20:34:48 -0700

    Isolated API function outside of event listener, added functionality for Starfleet, and started building API function for OpenWeather

--- app.js
+++ app.js
@@ -1,32 +1,48 @@
 const ul = document.querySelector('ul');
-const displayTemp = document.querySelector('#temp')
+const displayTemp = document.querySelector('#temp');
 const displayLocation = document.querySelector('#city');
+const displayHumidity = document.querySelector('#humidity');
 const logo = document.querySelector('#logo');
 
+let modifier = 0;
+let weather = 'https://api.weather.gov/stations/KSFO/observations';
 let city = "Starfleet";
-const vulcan = 'https://api.weather.gov/stations/KGEU/observations'; // Phoenix
-
-
-fetch(vulcan)
-    .then(data => data.json())
-    .then(data => {
-        logo.setAttribute('src', 'images/vulcan.jpg');
-        logo.setAttribute('alt', 'IDIC symbol');
-        displayLocation.textContent = `Location: ShiKahr, Vulcan`;
-        displayTemp.textContent = `Current Temperature: ${data.features[0].properties.temperature.value + 15}`;
-    });
+getWeather(weather);
 
 ul.addEventListener('click', (e) => {
-    console.log(e.target.textContent)
-    fetch(e.target.textContent)
-    .then(data => data.json())
-    .then(data => {
-        logo.setAttribute('src', 'images/vulcan.jpg');
-        logo.setAttribute('alt', 'IDIC symbol');
-        displayLocation.textContent = `Location: ShiKahr, Vulcan`;
-        displayTemp.textContent = `Current Temperature: ${data.features[0].properties.temperature.value + 15}`;
-    });
+    if(e.target.textContent === 'Vulcan'){
+        modifier = 15;
+        weather = 'https://api.weather.gov/stations/KGEU/observations';
+    }
+    else if(e.target.textContent === 'Starfleet'){
+        modifier = 0;
+        weather = 'https://api.weather.gov/stations/KSFO/observations';
+    }
+    else if(e.target.textContent === 'Federation'){
+        modifier = 0;
+        weather = 'http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=6e742ba939b15495d3b62b6883ce9c7';
+    }
+    city = e.target.textContent;
+    city !== 'Federation' ? getWeather(weather) : getInternational(weather);
 });
+
+function getInternational(weather){
+    fetch(weather)
+        .then(data => data.json())
+        .then(console.log(data));
+}
+
+function getWeather(weather){
+    fetch(weather)
+    .then(data => data.json())
+    .then(data => { 
+        console.log(data.features[0].properties.temperature.value);
+        logo.setAttribute('src', `images/${city}.jpg`);
+        logo.setAttribute('alt', `${city} symbol`);
+        displayLocation.textContent = `Location: ${city}`;
+        displayTemp.textContent = `Current Temperature: ${data.features[0].properties.temperature.value + modifier}`;
+    });
+}
 
 
 
