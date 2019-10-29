53109ec10cb7818f9be92ba85b58739742c5bdbe
jallend1 <jallend1@gmail.com>
2019-10-28 17:29:50 -0700

    Added functionality for Fererenginar and the Federation; Added current conditions icons (static because weather nets)

--- app.js
+++ app.js
@@ -3,44 +3,49 @@ const displayTemp = document.querySelector('#temp');
 const displayLocation = document.querySelector('#city');
 const displayHumidity = document.querySelector('#humidity');
 const logo = document.querySelector('#logo');
+const pic = document.querySelector('.pic');
 
 let modifier = 0;
 let weather = 'https://api.weather.gov/stations/KSFO/observations';
 let city = "Starfleet";
+let icon = 'images/Iconsun.svg';
 getWeather(weather);
 
 ul.addEventListener('click', (e) => {
     if(e.target.textContent === 'Vulcan'){
         modifier = 15;
         weather = 'https://api.weather.gov/stations/KGEU/observations';
+        icon = 'images/Iconhot.svg';
     }
     else if(e.target.textContent === 'Starfleet'){
         modifier = 0;
         weather = 'https://api.weather.gov/stations/KSFO/observations';
+        icon = 'images/Iconsun.svg';
     }
     else if(e.target.textContent === 'Federation'){
         modifier = 0;
-        weather = 'http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=6e742ba939b15495d3b62b6883ce9c7';
+        weather = 'http://api.openweathermap.org/data/2.5/weather?q=Paris,fr&APPID=26e742ba939b15495d3b62b6883ce9c7';
+        icon = 'images/Iconsun.svg';
+    }
+    else if(e.target.textContent === 'Ferenginar'){
+        modifier = 0;
+        weather = 'https://api.weather.gov/stations/kapf/observations';
+        icon = 'images/Iconrain.svg';
     }
     city = e.target.textContent;
-    city !== 'Federation' ? getWeather(weather) : getInternational(weather);
+    getWeather(weather);
 });
-
-function getInternational(weather){
-    fetch(weather)
-        .then(data => data.json())
-        .then(console.log(data));
-}
 
 function getWeather(weather){
     fetch(weather)
     .then(data => data.json())
     .then(data => { 
-        console.log(data.features[0].properties.temperature.value);
         logo.setAttribute('src', `images/${city}.jpg`);
         logo.setAttribute('alt', `${city} symbol`);
+        pic.setAttribute('src', `${icon}`)
         displayLocation.textContent = `Location: ${city}`;
-        displayTemp.textContent = `Current Temperature: ${data.features[0].properties.temperature.value + modifier}`;
+        city === 'Federation' ? displayTemp.textContent = `Current Temperature: ${Math.round(data.main.temp - 273.15)}`   // Converts from Kelvin (OpenWeatherMap API)
+            : displayTemp.textContent = `Current Temperature: ${Math.round(data.features[0].properties.temperature.value + modifier)}`;
     });
 }
 
@@ -48,4 +53,5 @@ function getWeather(weather){
 
 
     // https://wallpaperplay.com/board/starfleet-logo-wallpapers Starfleet
+    // http://i.imgur.com/5HQs8Ti.jpg Federation
     // https://cdn.wallpapersafari.com/73/49/YWl98f.jpg https://www.deviantart.com/wheatunrye   IDIC
